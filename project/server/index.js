bd06e55f7e3952d698a24d213d73d444593b0e21
nidhipawar5 <nidhipawar0504@gmail.com>
2021-08-05 13:42:34 +0530

    login feature added

---
+++ project/server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+import userRouter from './routes/user.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+app.use('/posts',postRoutes);
+app.use("/user", userRouter);
+
+app.get('/',(req,res)=>{
+    res.send('Hello to Memories API');
+});
+const CONNECTION_URL = 'mongodb+srv://nidhipawar:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT ;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT,()=> console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify',false);    
